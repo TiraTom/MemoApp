d6fde87a38fa53e29cd54379e357848657354403
OsadaMasako <217bssr@gmail.com>
2019-01-21 21:18:18 +0900

    新規作成・画面のたたき作成・Modelの作成・移行プロジェクトの準備

--- MemoApp/Views/MainPage.xaml.cs
+++ MemoApp/Views/MainPage.xaml.cs
@@ -15,16 +15,20 @@ using Windows.UI.Xaml.Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
-namespace MemoApp
+namespace MemoApp.Views
 {
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
     public sealed partial class MainPage : Page
     {
+		public ViewModels.MainPageViewModel ViewModel { get; private set; } = new ViewModels.MainPageViewModel();
+
         public MainPage()
         {
             this.InitializeComponent();
+			ViewModel.Initialize(this);
+			this.DataContext = ViewModel;
         }
     }
 }
---
+++ MempApp.Model/Model.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MempApp.Model
+{
+	public class MemoAppContext : DbContext
+	{
+		public DbSet<HashItem> HashItemList {get; set;}
+		public DbSet<EachTask> TaskList { get; set; }
+		public DbSet<Memo> MemoList { get; set; }
+	}
+
+	public class Memo
+	{
+		public string ID { get; }
+		public DateTime CreateTime { get; set; }
+		public DateTime UpdateTime { get; set; }
+		public string Content { get; set; }
+	}
+	public class HashItem
+	{
+		public string ID { get; }
+		public string Name { get; set; } = default;
+		public DateTime UsedTime { get; set; } = default;
+	}
+	public class EachTask
+	{
+		public string ID { get; }
+		private string taskName = string.Empty;
+		public List<TimeInfo> TimeInfoList { get; set; }
+		public DateTime DeadLine { get; set; } = default;
+		public DateTime PlanDate { get; set; } = default;
+
+		public string TaskName
+		{
+			get { return this.taskName; }
+			set { this.taskName = value; }
+		}
+
+		public class TimeInfo
+		{
+			private DateTime start;
+			private DateTime stop;
+
+			public DateTime Start { get; set; }
+			public DateTime Stop { get; set; }
+			public TimeSpan ElapsedTime()
+			{
+				return (Start != null && Stop != null) ? Stop.Subtract(Start) : new TimeSpan(0);
+			}
+		}
+
+		public bool IsOver()
+		{
+			return DeadLine.CompareTo(DateTime.Now) == 0 ? false : true;
+		}
+	}
+}
